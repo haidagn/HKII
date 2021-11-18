8cbe4c51acd89f5a64d222be2f180bb2559cbffd
haidagn <84432461+haidagn@users.noreply.github.com>
2021-11-19 03:43:34 +0700

    rp navbar

--- assets/script/app.js
+++ assets/script/app.js
@@ -29,60 +29,86 @@ $('.butn-primary_slider').click(function () {
     // Parameters has to be in square bracket '[]'
     owl.trigger('prev.owl.carousel', [300]);
 })
-   // disabled login
-        const loginBtn = document.getElementById('loginbtn')
-        const input = document.getElementById('email')
+// disabled login
+const loginBtn = document.getElementById('loginbtn')
+const input = document.getElementById('email')
 
-        input.addEventListener('keypress', (e) => {
-            const value = e.currentTarget.value;
-            if (value === "") {
-                loginBtn.disabled = true;
-            } else {
-                loginBtn.disabled = false;
-            }
-        })
+input.addEventListener('keypress', (e) => {
+    const value = e.currentTarget.value;
+    if (value === "") {
+        loginBtn.disabled = true;
+    } else {
+        loginBtn.disabled = false;
+    }
+})
 
 
-        // bật login
-        const openLoginBtn = document.querySelectorAll('[data-login-target]');
-        const closeLoginBtn = document.querySelectorAll('[data-login-close]');
-        const checkMark = document.getElementById('successanimation')
+// bật login
+const openLoginBtn = document.querySelectorAll('[data-login-target]');
+const closeLoginBtn = document.querySelectorAll('[data-login-close]');
+const checkMark = document.getElementById('successanimation')
 
-        openLoginBtn.forEach(button => {
-            button.addEventListener('click', () => {
-                const login = document.querySelector(button.dataset.loginTarget)
-                openLogin(login)
-            })
-        })
-        closeLoginBtn.forEach(button => {
-            button.addEventListener('click', () => {
-                const login = button.closest('.body-login')
-                closeLogin(login)
-            })
-        })
-        function openLogin(login) {
-            if (login == null) return
-            login.classList.add('login-ative')
-            checkMark.classList.add('d-none')
-            checkMark.classList.remove('d-block')
-        }
-        function closeLogin(login) {
-            if (login == null) return
-            login.classList.remove('login-ative')
-            checkMark.classList.remove('d-none')
-            checkMark.classList.add('d-block')
-        }
+openLoginBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const login = document.querySelector(button.dataset.loginTarget)
+        openLogin(login)
+    })
+})
+closeLoginBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const login = button.closest('.body-login')
+        closeLogin(login)
+    })
+})
+function openLogin(login) {
+    if (login == null) return
+    login.classList.add('login-ative')
+    checkMark.classList.add('d-none')
+    checkMark.classList.remove('d-block')
+}
+function closeLogin(login) {
+    if (login == null) return
+    login.classList.remove('login-ative')
+    checkMark.classList.remove('d-none')
+    checkMark.classList.add('d-block')
+}
 
-        // đăng Nhập
-        const openSucBtn = document.querySelectorAll('[data-success-target]');
+// đăng Nhập
+const openSucBtn = document.querySelectorAll('[data-success-target]');
 
-        openSucBtn.forEach(button => {
-            button.addEventListener('click', () => {
-                const success = document.querySelector(button.dataset.successTarget)
-                openSuc(success)
-            })
-        })
-        function openSuc(success) {
-            if (success == null) return
-            success.classList.add('success-ative')
-        }
+openSucBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const success = document.querySelector(button.dataset.successTarget)
+        openSuc(success)
+    })
+})
+function openSuc(success) {
+    if (success == null) return
+    success.classList.add('success-ative')
+}
+
+
+// bat tat menu mobile 
+const openNavBtn = document.querySelectorAll('[data-nav-target]');
+const closeNavBtn = document.querySelectorAll('[data-nav-close]');
+
+openNavBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const navs = document.querySelector(button.dataset.navTarget)
+        openNav(navs)
+    })
+})
+closeNavBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const navs = button.closest('.navs-links')
+        closeNav(navs)
+    })
+})
+function openNav(navs) {
+    if (navs == null) return
+    navs.classList.add('nav-ative')
+}
+function closeNav(navs) {
+    if (navs == null) return
+    navs.classList.remove('nav-ative')
+}
