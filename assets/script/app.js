a03b4ffba70181fabe02b4cccf136d9a20e91005
haidagn <84432461+haidagn@users.noreply.github.com>
2021-11-19 02:09:02 +0700

    up favicon
    du` ma' no' met. moi~ du~ dan`

--- assets/script/app.js
+++ assets/script/app.js
@@ -29,29 +29,60 @@ $('.butn-primary_slider').click(function () {
     // Parameters has to be in square bracket '[]'
     owl.trigger('prev.owl.carousel', [300]);
 })
+   // disabled login
+        const loginBtn = document.getElementById('loginbtn')
+        const input = document.getElementById('email')
 
-// $('.section-5 .owl-carousel').owlCarousel({
-//     loop: true,
-//     nav: false,
-//     dots: false,
-//     autoWidth: false,
-//     mergeFit: false,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 3
-//         },
-//         1000: {
-//             items: 4
-//         }
-//     }
-// })
+        input.addEventListener('keypress', (e) => {
+            const value = e.currentTarget.value;
+            if (value === "") {
+                loginBtn.disabled = true;
+            } else {
+                loginBtn.disabled = false;
+            }
+        })
 
-// cuon trang section-6
-// const section6 = document.getElementById('section6');
-// window.addEventListener('scroll', function () {
-//     let offset = window.pageXOffset;
-//     section6.style.backgroundPositionY = offset * 0.5 + 'px';
-// })
+
+        // bật login
+        const openLoginBtn = document.querySelectorAll('[data-login-target]');
+        const closeLoginBtn = document.querySelectorAll('[data-login-close]');
+        const checkMark = document.getElementById('successanimation')
+
+        openLoginBtn.forEach(button => {
+            button.addEventListener('click', () => {
+                const login = document.querySelector(button.dataset.loginTarget)
+                openLogin(login)
+            })
+        })
+        closeLoginBtn.forEach(button => {
+            button.addEventListener('click', () => {
+                const login = button.closest('.body-login')
+                closeLogin(login)
+            })
+        })
+        function openLogin(login) {
+            if (login == null) return
+            login.classList.add('login-ative')
+            checkMark.classList.add('d-none')
+            checkMark.classList.remove('d-block')
+        }
+        function closeLogin(login) {
+            if (login == null) return
+            login.classList.remove('login-ative')
+            checkMark.classList.remove('d-none')
+            checkMark.classList.add('d-block')
+        }
+
+        // đăng Nhập
+        const openSucBtn = document.querySelectorAll('[data-success-target]');
+
+        openSucBtn.forEach(button => {
+            button.addEventListener('click', () => {
+                const success = document.querySelector(button.dataset.successTarget)
+                openSuc(success)
+            })
+        })
+        function openSuc(success) {
+            if (success == null) return
+            success.classList.add('success-ative')
+        }
